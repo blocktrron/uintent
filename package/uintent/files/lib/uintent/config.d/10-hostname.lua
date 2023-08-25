987297434e1dd4417e391f1f3080ff89398c0c8c
Tom Herbers <mail@tomherbers.de>
2023-08-25 02:05:04 +0200

    uintent: replace mac address placeholder in hostname
    closes: blocktrron/uintent#9

--- package/uintent/files/lib/uintent/config.d/10-hostname.lua
+++ package/uintent/files/lib/uintent/config.d/10-hostname.lua
@@ -5,5 +5,10 @@ local util = require("uintent.util")
 
 local profile = util.get_profile()
 
-uci:set("system", uci:get_first("system", "system"), "hostname", profile["hostname"])
+local hostname = profile["hostname"]
+
+-- replace placeholders
+hostname = string.gsub(hostname, "{mac}", string.gsub(util.get_primary_mac(), ":", ""))
+
+uci:set("system", uci:get_first("system", "system"), "hostname", hostname)
 uci:commit("system")
